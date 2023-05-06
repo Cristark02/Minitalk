de32d37ef908f0981551646c117605a5d5862447
Cristark <mihaimita02@gmail.com>
2023-05-06 13:25:28 +0200

    libreria mejorada

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,12 +6,13 @@
 /*   By: mmita <mmita@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 16:46:37 by mmita             #+#    #+#             */
-/*   Updated: 2023/04/30 16:46:40 by mmita            ###   ########.fr       */
+/*   Updated: 2023/05/02 22:26:16 by mmita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
+# define _GNU_SOURCE
 # include <signal.h>
 # include "../libft/libft.h"
 
